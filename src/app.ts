8343d19b653d4c0b30729059d31fc3a08815bb49
safin33221 <safin33221@gmail.com>
2026-05-10 03:30:35 +0600

    refactor: update root endpoint response to return structured JSON

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-import express, { type Application, type Request, type Response } from 'express'
+import express, { type Application, type Request, type Response } from 'express';
 
 
 
-const app: Application = express()
-const port = 5000
+const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.status(200).json({
+    success: true,
+    message: 'API is running successfully',
+    data: {
+      name: 'FPI Campus Management System',
+      version: '1.0.0'
+    }
+  });
+});
 
 
-
-export default app
+export default app;
