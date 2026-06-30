f1f3814fc1c4d8f57f4602d9588c59edd864e474
safin33221 <safin33221@gmail.com>
2026-07-01 02:22:46 +0600

    feat: Enhance user profile retrieval with additional fields and nested selections for student and teacher details

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -22,18 +22,75 @@ const getMyProfile = async (userId: string) => {
             id: true,
             name: true,
             email: true,
+            loginId: true,
             role: true,
+
             isVerified: true,
             isActive: true,
+            mustChangePassword: true,
+            lastLogin: true,
+
+            profileImage: true,
+            gender: true,
+            dateOfBirth: true,
+            phone: true,
+            address: true,
+            city: true,
+            district: true,
+
             createdAt: true,
             updatedAt: true,
+
+            student: {
+                select: {
+                    id: true,
+                    name: true,
+                    roll: true,
+                    registrationNo: true,
+                    session: true,
+                    batch: true,
+                    phone: true,
+                    dob: true,
+
+                    department: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+
+                    semester: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                },
+            },
+
+            teacher: {
+                select: {
+                    id: true,
+                    teacherId: true,
+                    designation: true,
+                    qualification: true,
+                    joiningDate: true,
+                    experienceYears: true,
+
+                    department: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                },
+            },
         },
     });
 
     if (!user) {
         throw new ApiError(httpCode.NOT_FOUND, "User not found");
     }
-
 
     return user;
 };
